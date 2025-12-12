996aa2b329ec136e4b94b9d9b2a9cf3c2985bca3
Daniel Vartanian <danielvartan@proton.me>
2025-12-12 19:02:58 -0300

    Add an exploratory analysis section

---
+++ R/loop.R
@@ -0,0 +1,31 @@
+library(cli)
+library(fs)
+library(groomr) # github.com/danielvartan/groomr
+library(here)
+
+years <- c(2009:2019, 2021:2023)
+
+cli_progress_bar(
+  name = "Rendering files",
+  total = length(years),
+  clear = FALSE
+)
+
+for (i in years) {
+  replace_in_file(
+    file = here("index.qmd"),
+    pattern = "^year <- \\d{4}$",
+    replacement = paste0("year <- ", i)
+  )
+
+  system("quarto render")
+
+  dir_ls(here("data")) |> file_delete()
+  dir_ls(here("data-raw")) |> file_delete()
+
+  gc()
+
+  cli_progress_update()
+}
+
+cli_progress_done()
